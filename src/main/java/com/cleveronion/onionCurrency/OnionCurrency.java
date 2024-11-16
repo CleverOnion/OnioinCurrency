ae1b0c4a43e9c40a80d71eb5837c0e6cd70f6dde
CleverOnion <cleveronion1025@gmail.com>
2024-11-16 18:40:21 +0800

    初始化项目

---
+++ src/main/java/com/cleveronion/onionCurrency/OnionCurrency.java
@@ -0,0 +1,19 @@
+package com.cleveronion.onionCurrency;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OnionCurrency extends JavaPlugin {
+
+    public static 
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
